265cc7562260649f788f63b7a926ad9f4ef7a71f
Ibrahima <iab.jr10@gmail.com>
2023-06-12 18:31:01 -0400

    Updated HTML, CSS and JavaScript files. Added questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+/*
+Here are the questions for the quiz
+1. Inside which HTML element do we put the JavaScript?
+a. <script>*             b. <js>
+c. <javascript>         d. <scripting>
+
+2. What is the correct JavaScript syntax to change the content of the HTML element below?
+a. document.getElementById("demo").innerHTML = "Hello World!";*
+b. #demo.innerHTML = "Hello World!";
+c. document.getElement("p").innterHTML = "Hello World!";
+d. document.getElementByName("p").innerHTML = "Hello World!";
+
+3. Where is the correct place to insert a JavaScript?
+a. The <body> section
+b. Both the <body> section and the <head> section are correct. *
+c. The <head> section
+
+4. What is the correct syntax for reffering to an external script called "abc.js"?
+a. <script src="abc.js"> *
+b. <script name="abc.js">
+c. <script href="abc.js">
+
+5. The external JavaScript must contain the <script> tag.
+a. False *
+b. True
+
+6. How do you write "Hello World!" in an alert box?
+a. alert("Hello World!"); *
+b. msg("Hello World!");
+c. alertBox("Hello World!");
+d. msgBox("Hello World!");
+*/
--- script.js
+++ script.js
@@ -8,12 +8,13 @@ Go back button to restart quiz and clear scores button to wipe scores
 
 
 */
-
+// Manipulates the countdown id
 var timerEl = document.getElementById('countdown');
 var mainAreaEl = document.getElementById('main-area');
 var beginEl = document.getElementById('start-button');
 
-var timeLeft = 150;
+var timeLeft = 5;
+var timeInterval;
 function setTimerText () {
     timerEl.textContent = timeLeft;
 }
@@ -21,24 +22,48 @@ function setTimerText () {
 
 function countdown() {
     setTimerText();
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
         if (timeLeft > 0) {
-            timerEl.textcontent = 'Time: ' + timeLeft;
             timeLeft--;
+            timerEl.textContent = timeLeft;
         } else {
+            timerEl.textContent = '';
             clearInterval(timeInterval);
             endAttempt();
         }
     }, 1000);
 }
 
+// Creating go back button
+var goBackButton = document.createElement('Button');
+goBackButton.id = 'backButton';
+goBackButton.innerHTML = 'Go Back';
+goBackButton.style.background = 'grey';
+
+// creating clear highscore button
+// var btn = document.createElement('Button');
+// btn.setAttribute('type', 'button');
+// btn.setAttribute('value', 'Clear Highscores');
+// btn.setAttribute('background-color', 'grey')
+
+var clearScoreBtn = document.createElement('Button');
+goBackButton.id = 'clearScore';
+goBackButton.innerHTML = 'Clear Highscores';
+goBackButton.style.background = 'grey';
+
+
 function endAttempt() {
-    mainAreaEl.textContent = cancelAnimationFrame();
+    mainAreaEl.textContent = 'Highscores';
+    mainAreaEl.appendChild(goBackButton);
+    // document.body.appendChild(clearScoreBtn);
+
 
 }
+
+
+
+beginEl.addEventListener('click', countdown);
 
 // beginEl.addEventListener('click', function() {
 //     timerEl.textContent = 'Time: ' + timeLeft;
 // });
-
-beginEl.addEventListener('click', countdown());
