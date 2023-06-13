481cfcad6777b7c99141f31e4d86aff4751d8685
Ibrahima <iab.jr10@gmail.com>
2023-06-12 23:55:57 -0400

    Added more code to HTML, CSS, and JavaScript files

--- script.js
+++ script.js
@@ -12,9 +12,27 @@ Go back button to restart quiz and clear scores button to wipe scores
 var timerEl = document.getElementById('countdown');
 var mainAreaEl = document.getElementById('main-area');
 var beginEl = document.getElementById('start-button');
+var question1 = document.getElementById('question1');
+var q1answer = document.getElementById('q1-answer');
+var question2 = document.getElementById('question2');
+var q2answer = document.getElementById('q2-answer');
+var question3 = document.getElementById('question3');
+var q3answer = document.getElementById('q3-answer');
+var question4 = document.getElementById('question4');
+var q4answer = document.getElementById('q4-answer');
+var question5 = document.getElementById('question5');
+var q5answer = document.getElementById('q5-answer');
 
-var timeLeft = 5;
+var timeLeft = 70;
 var timeInterval;
+
+function endAttempt() {
+    mainAreaEl.setAttribute('display', 'none');
+}
+
+function wrongAnswer() {
+    timeLeft-= 10;
+}
 function setTimerText () {
     timerEl.textContent = timeLeft;
 }
@@ -26,6 +44,8 @@ function countdown() {
         if (timeLeft > 0) {
             timeLeft--;
             timerEl.textContent = timeLeft;
+            // Hide main area and show questions 1
+            mainAreaEl.setAttribute('display', 'none');
         } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
@@ -34,36 +54,16 @@ function countdown() {
     }, 1000);
 }
 
-// Creating go back button
-var goBackButton = document.createElement('Button');
-goBackButton.id = 'backButton';
-goBackButton.innerHTML = 'Go Back';
-goBackButton.style.background = 'grey';
-
-// creating clear highscore button
-// var btn = document.createElement('Button');
-// btn.setAttribute('type', 'button');
-// btn.setAttribute('value', 'Clear Highscores');
-// btn.setAttribute('background-color', 'grey')
-
-var clearScoreBtn = document.createElement('Button');
-goBackButton.id = 'clearScore';
-goBackButton.innerHTML = 'Clear Highscores';
-goBackButton.style.background = 'grey';
-
-
-function endAttempt() {
-    mainAreaEl.textContent = 'Highscores';
-    mainAreaEl.appendChild(goBackButton);
-    // document.body.appendChild(clearScoreBtn);
-
-
-}
-
-
-
 beginEl.addEventListener('click', countdown);
 
-// beginEl.addEventListener('click', function() {
-//     timerEl.textContent = 'Time: ' + timeLeft;
-// });
+// Add event listener to correct answer of question 1 and clear screen
+// and display question 2.
+q1answer.addEventListener('click', function);
+
+q2answer.addEventListener('click', );
+
+q3answer.addEventListener('click', );
+
+q4answer.addEventListener('click', );
+
+q5answer.addEventListener('click', );
